386272a32bab6e2deb99ea24a3b06a522e7f61ad
Venkata C <vc@chebolu.net>
2018-12-24 19:32:42 -0500

    correction

--- site/js/script.js
+++ site/js/script.js
@@ -92,7 +92,7 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-
+  console.log("buildAndShowHomeHTML called " + categories.length);
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
